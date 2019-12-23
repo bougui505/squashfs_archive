30287db22d55ab48d6a5cad559d0fe54a7f9f8c6
Guillaume Bouvier <bougui@mantrisse.bis.pasteur.fr>
2019-12-23 14:50:46 +0100

    Better handling of options 	modified:   unsquash.sh

--- unsquash.sh
+++ unsquash.sh
@@ -19,16 +19,20 @@ if [ "$#" -lt 1 ]; then
     exit
 fi
 
+ACTION="MOUNT"
 if [ "$1" = "-u" ]; then
+    ACTION="UNMOUNT"
     shift
-    ARCHIVE="$1"
-    MNTDIR="$ARCHIVE:r"
+fi
+
+ARCHIVE="$1"
+MNTDIR="$ARCHIVE:r"
+
+if [ $ACTION = "UNMOUNT" ]; then
     sudo umount $MNTDIR \
         && echo "$MNTDIR unmounted" \
         && rmdir "$MNTDIR"
 else
-    ARCHIVE="$1"
-    MNTDIR="$ARCHIVE:r"
     mkdir $MNTDIR \
         && sudo mount "$ARCHIVE" "$MNTDIR" -t squashfs -o loop \
         && echo "$ARCHIVE mounted in $MNTDIR"
